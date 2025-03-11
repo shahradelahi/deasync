1366c8dbbf3a45bbc2f770b83b5791093da3200e
Shahrad Elahi <shahrad@litehex.com>
2025-03-11 19:30:44 +0000

    fix

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
+  bundle: false,
   clean: true,
   dts: true,
-  entry: ['index.ts'],
-  external: [/\.node$/],
+  entry: ['index.ts', 'bindings.js'],
   format: ['cjs'],
   outDir: 'dist',
 });
